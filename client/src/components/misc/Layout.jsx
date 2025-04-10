0d4684a4a1df79980e850590e1bdcbb52e5522ba
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2025-04-10 23:20:17 +0800

    created navbar

--- client/src/components/misc/Layout.jsx
+++ client/src/components/misc/Layout.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
+import Navbar from './Navbar';
 
 function Layout({ children }) {
     return (
         <div className="bg-bg-primary min-h-screen min-w-screen">
+            <Navbar />
             { children }
         </div>
     )
---
+++ client/src/components/misc/Navbar.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+
+const Navbar = () => {
+    const [currentPath, setCurrentPath] = useState("")
+
+    useEffect(() => {
+        const setPath = () => {
+            setCurrentPath(window.location.pathname);
+        };
+
+        setPath();
+
+        window.addEventListener("popstate", () => {
+            setPath();
+        })
+
+        return () => {
+            window.removeEventListener("popstate", setPath);
+        }
+    }, [])
+
+    const isActive = (path) => {
+        return currentPath == path;
+    }
+
+    return (
+        <>
+        <div className="py-10 px-15">
+            <div className="content-between flex justify-between">
+                <div className="content-start flex gap-20">
+                    <h1
+                        className="font-bold text-shadow text-shadow-lg/10 clickable flex items-center"
+                        onClick={() => window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")}
+                    >
+                        Gym Thing
+                    </h1>
+
+                    <ul className="flex items-center gap-20">
+                        <li>
+                            <h2
+                                className={`clickable ${isActive("/progress") ? "" : "opacity-50"}`}
+                                onClick={() => window.location.href = "/progress"}
+                            >
+                                Progress
+                            </h2>
+                        </li>
+
+                        <li>
+                            <h2
+                                className={`clickable ${isActive("exercises") ? "" : "opacity-50"}`}
+                                onClick={() => window.location.href = "/exercises"}
+                            >
+                                Exercises
+                            </h2>
+                        </li>
+                    </ul>
+
+                </div>
+                {/* left */}
+
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Navbar;
