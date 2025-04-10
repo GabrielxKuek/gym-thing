93096fe89626d290ca29c5e2fe7cfae9a33832fc
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2025-04-10 23:17:09 +0800

    misc update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // pages
+  // handler
 import TestPage from './pages/TestPage'
 import HomePage from './pages/HomePage'
 import ErrorPage from './pages/ErrorPage'
+  // features
+import ProgressPage from './pages/ProgressPage'
+
+// elements
 import Layout from './components/misc/Layout';
 
 function App() {
@@ -12,8 +17,14 @@ function App() {
     <BrowserRouter>
       <Layout>
         <Routes>
+          {/* handler routes */}
           <Route path="/" element={<HomePage />} />
           <Route path="/test" element={<TestPage />} />
+
+          {/* feature routes */}
+          <Route path="/progress" element={<ProgressPage />}></Route>
+
+          {/* catch all 404 page */}
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </Layout>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import './styles/index.css'
 import './styles/typography.css'
+import './styles/style.css'
 
 import App from './App.jsx'
 
