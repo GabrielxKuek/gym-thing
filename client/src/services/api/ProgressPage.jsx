10e1f727b6a95b54ad75e315a08862cc7b121b3c
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2025-04-12 13:39:17 +0800

    feat: dummy exercise data for progresspage

---
+++ client/src/services/api/ProgressPage.jsx
@@ -0,0 +1,217 @@
+// dummy data for heatmap
+export function getUserExerciseProgress() {
+    return [
+        {
+            "date": "2025-03-31",
+            "time": 120, // in minutes
+            "exercises": [
+                {
+                    "name": "Bench Press",
+                    "reps": 6,
+                    "sets": 3,
+                    "weight": 28
+                },
+                {
+                    "name": "Lateral Raise",
+                    "reps": 8,
+                    "sets": 3,
+                    "weight": 12
+                },
+                {
+                    "name": "Running",
+                    "time": 60,
+                    "distance": 2400, // in meters
+                    "type": "treadmill",
+                    "speed": 9,
+                    "incline": 0
+                }
+            ]
+        },
+        {
+            "date": "2025-04-01",
+            "time": 90, // in minutes
+            "exercises": [
+                {
+                    "name": "Squat",
+                    "reps": 8,
+                    "sets": 4,
+                    "weight": 50
+                },
+                {
+                    "name": "Deadlift",
+                    "reps": 5,
+                    "sets": 3,
+                    "weight": 70
+                },
+                {
+                    "name": "Running",
+                    "time": 30,
+                    "distance": 1200, // in meters
+                    "type": "treadmill",
+                    "speed": 8,
+                    "incline": 1
+                }
+            ]
+        },
+        {
+            "date": "2025-04-02",
+            "time": 60, // in minutes
+            "exercises": [
+                {
+                    "name": "Push Up",
+                    "reps": 10,
+                    "sets": 3,
+                    "weight": 0
+                },
+                {
+                    "name": "Pull Up",
+                    "reps": 5,
+                    "sets": 3,
+                    "weight": 0
+                },
+                {
+                    "name": "Running",
+                    "time": 45,
+                    "distance": 1800, // in meters
+                    "type": "treadmill",
+                    "speed": 7,
+                    "incline": 2
+                }
+            ]
+        },
+        {
+            "date": "2025-04-03",
+            "time": 75, // in minutes
+            "exercises": [
+                {
+                    "name": "Leg Press",
+                    "reps": 10,
+                    "sets": 4,
+                    "weight": 100
+                },
+                {
+                    "name": "Bicep Curl",
+                    "reps": 12,
+                    "sets": 3,
+                    "weight": 15
+                },
+                {
+                    "name": "Running",
+                    "time": 20,
+                    "distance": 800, // in meters
+                    "type": "treadmill",
+                    "speed": 6,
+                    "incline": 3
+                }
+            ]
+        },
+        {
+            "date": "2025-04-04",
+            "time": 45, // in minutes
+            "exercises": [
+                {
+                    "name": "Leg Extension",
+                    "reps": 12,
+                    "sets": 3,
+                    "weight": 40
+                },
+                {
+                    "name": "Leg Curl",
+                    "reps": 10,
+                    "sets": 3,
+                    "weight": 30
+                },
+                {
+                    "name": "Running",
+                    "time": 15,
+                    "distance": 600, // in meters
+                    "type": "treadmill",
+                    "speed": 5,
+                    "incline": 4
+                }
+            ]
+        },
+        {
+            "date": "2025-04-05",
+            "time": 0,
+            "exercises": []
+        },
+        {
+            "date": "2025-04-06",
+            "time": 0,
+            "exercises": []
+        },
+        {
+            "date": "2025-04-07",
+            "time": 180,
+            "exercises": [
+                {
+                    "name": "Cycling",
+                    "time": 120,
+                    "distance": 30, // in kilometers
+                    "type": "bicycle",
+                    "speed": 20
+                }
+            ]
+        },
+        {
+            "date": "2025-04-08",
+            "time": 0,
+            "exercises": []
+        },
+        {
+            "date": "2025-04-09",
+            "time": 80,
+            "exercises": [
+                {
+                    "name": "Bicep Curl",
+                    "reps": 10,
+                    "sets": 3,
+                    "weight": 20
+                },
+                {
+                    "name": "Burpees",
+                    "reps": 15,
+                    "sets": 3,
+                    "weight": 0
+                }
+            ]
+        },
+        {
+            "date": "2025-04-10",
+            "time": 40,
+            "exercises": [
+                {
+                    "name": "Plank",
+                    "time": 5,
+                    "sets": 3,
+                    "weight": 0
+                },
+                {
+                    "name": "Mountain Climber",
+                    "reps": 20,
+                    "sets": 3,
+                    "weight": 0
+                }
+            ]
+        },
+        {
+            "date": "2025-04-11",
+            "time": 100,
+            "exercises": [
+                {
+                    "name": "Leg Press",
+                    "reps": 10,
+                    "sets": 4,
+                    "weight": 120
+                },
+                {
+                    "name": "Shoulder Press",
+                    "reps": 8,
+                    "sets": 3,
+                    "weight": 30
+                }
+            ]
+        }
+    ]
+}
